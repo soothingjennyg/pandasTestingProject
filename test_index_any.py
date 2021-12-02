7a2689c9360c99ce4c4ab8b20604a95d4c436c07
susmitha <peddi.susmitharao@gmail.com>
2021-12-02 11:09:56 +0100

    test cases

---
+++ test_index_any.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import unittest
+
+class TestIndexAny(unittest.TestCase):
+    def setUp(self):
+        self.series1 = pd.Index([3, 4, 6])
+        self.series2 = pd.Index([1, 1, 2, 0])
+        self.series3 = pd.Index([0, 0, 0])
+        self.series4 = pd.Index([])
+        self.series5 = pd.Index([6,0,0,0])
+
+
+    def test_Index_Any(self):
+        self.assertTrue(self.series1.any() == True)
+        self.assertTrue(self.series2.any() == True)
+        self.assertTrue(self.series3.any() == False)
+        self.assertTrue(self.series4.any() == False)
+        self.assertTrue(self.series5.any() == True)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
