9a2c389d9e79f34200f70c35f4cb521b526d3e55
Sukhavas <90395368+Sukhavas@users.noreply.github.com>
2021-12-03 17:34:26 +0100

    Update test_index_any.py

--- test_index_any.py
+++ test_index_any.py
@@ -8,6 +8,7 @@ class TestIndexAny(unittest.TestCase):
         self.series3 = pd.Index([0, 0, 0])
         self.series4 = pd.Index([])
         self.series5 = pd.Index([6,0,0,0])
+        self.series6 = pd.Index([9,8,6,0,1,0])
 
 
     def test_Index_Any(self):
@@ -16,6 +17,7 @@ class TestIndexAny(unittest.TestCase):
         self.assertTrue(self.series3.any() == False)
         self.assertTrue(self.series4.any() == False)
         self.assertTrue(self.series5.any() == True)
+         self.assertTrue(self.series6.any() == True)
 
 
 if __name__ == '__main__':
