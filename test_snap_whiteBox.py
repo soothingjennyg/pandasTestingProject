1f8d8e2b2dac7631e10555be6887774a1c5844a3
jonas <jonas.kittelberger@gmail.com>
2021-12-07 14:32:17 +0100

    implemented function to capture fullfilled prime paths

--- test_snap_whiteBox.py
+++ test_snap_whiteBox.py
@@ -48,6 +48,22 @@ class TestSnap(unittest.TestCase):
         self.assertTrue(sn.equals(self.index1W))
 
 
+    def test_PrimePaths(self):
+        sn = snapStorePath(self.index1, freq="D")
+        self.assertTrue(sn.equals(self.index1))
+
+        sn = snapStorePath(self.index1, freq="W")
+        self.assertTrue(sn.equals(self.index1W))
+
+        sn = snapStorePath(self.index2, freq="D")
+        self.assertTrue(sn.equals(self.index2))
+
+        sn = snapStorePath(self.index3, freq="W")
+        self.assertTrue(sn.equals(self.index3))
+
+        checkComplete()
+
+
 
 def snap(self, freq="S") -> DatetimeIndex:
     print("\nNode 0")
@@ -73,5 +89,71 @@ def snap(self, freq="S") -> DatetimeIndex:
         snapped[i] = s
 
     print("Node 3\n")
+    dta = DatetimeArray(snapped, dtype=self.dtype)
+    return DatetimeIndex._simple_new(dta, name=self.name)
+
+
+
+primePaths = [[0, 1, 3],    #size 3
+
+              [2, 7, 1, 2], [2, 7, 1, 3], [6, 7, 1, 3], [7, 1, 2, 7],   #size 4
+
+              [0, 1, 2, 4, 5, 7], [0, 1, 2, 4, 6, 7], [1, 2, 4, 5, 7, 1],
+              [1, 2, 4, 6, 7, 1], [2, 4, 5, 7, 1, 2], [2, 4, 5, 7, 1, 3],
+              [2, 4, 6, 7, 1, 2], [2, 4, 6, 7, 1, 3], [4, 5, 7, 1, 2, 4],
+              [4, 6, 7, 1, 2, 4], [5, 7, 1, 2, 4, 5], [5, 7, 1, 2, 4, 6],
+              [6, 7, 1, 2, 4, 5], [6, 7, 1, 2, 4, 6], [7, 1, 2, 4, 5, 7], [7, 1, 2, 4, 6, 7]    #size 6
+              ]
+
+
+def checkComplete():
+    missing = []
+    fullfilled = []
+    for primePath in primePaths:
+        primePathStr = "".join(str(num) for num in primePath)
+        found = False
+        for executedPath in executedPaths:
+            executedPathStr = "".join(str(num) for num in executedPath)
+            if primePathStr in executedPathStr:
+                found = True
+                break
+
+        if found:
+            fullfilled.append(primePath)
+        else:
+            missing.append(primePath)
+
+    print("\n Missing prime paths: ", missing)
+    print("\n Fulfilled prime paths: ", fullfilled)
+    return missing
+
+
+executedPaths = []
+def snapStorePath(self, freq="S") -> DatetimeIndex:
+    path = []
+    path.append(0)
+    path.append(1)
+    freq = to_offset(freq)
+
+    snapped = np.empty(len(self), dtype=DT64NS_DTYPE)
+
+    for i, v in enumerate(self):
+        path.append(2)
+        s = v
+        if not freq.is_on_offset(s):
+            path.append(4)
+            t0 = freq.rollback(s)
+            t1 = freq.rollforward(s)
+            if abs(s - t0) < abs(t1 - s):
+                path.append(5)
+                s = t0
+            else:
+                path.append(6)
+                s = t1
+        path.append(7)
+        snapped[i] = s
+
+    path.append(3)
+    executedPaths.append(path)
     dta = DatetimeArray(snapped, dtype=self.dtype)
     return DatetimeIndex._simple_new(dta, name=self.name)
