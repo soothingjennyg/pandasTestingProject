93154c8e245be22ae40a8465a5281a33ce730f9f
Sukhavas <90395368+Sukhavas@users.noreply.github.com>
2021-12-08 18:35:44 +0100

    Added summary

--- test_series_sum.py
+++ test_series_sum.py
@@ -3,6 +3,23 @@ import numpy as np
 import unittest
 
 class TestSeriesSum(unittest.TestCase):
+    """
+    Test the pandas.series.sum function
+    sum() will be used to get the sum of the values for the requested axis.
+    it will return the sum of the requested series
+    paramerets:
+    level:int or level name or default none
+    if the axis is a multiindex(hierarchical),count along a perticular level,collapsing into a smaller series.
+    returns
+    sum of the values of requested axis.
+    if you have an series[4,2,0,8]for names['dog','fish','cat','mouse']it will return
+    dog    4
+    fish   2 
+    cat    0 
+    mouse  8
+    sum() 14
+    """
+    
     def setUp(self):
         self.series1 = pd.Series([], dtype='int')
         self.series2 = pd.Series([1,2,3,4])
